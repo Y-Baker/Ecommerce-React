d8cd98bac60e16868b84471f290871f4bbd27aaf
Yousef <yuossefbakier@gmail.com>
2024-05-18 00:49:54 +0300

    Handle Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/home/home';
+import Header from './components/header/header';
+import ProductPage from './components/product/product-page';
+import Footer from './components/footer/footer';
 
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
+  return <div className='overflow-hidden'>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/product/:id' element={<ProductPage />} />
+
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </div>;
 };
 
 export default App;
